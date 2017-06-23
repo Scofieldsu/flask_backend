5b910ac685c3e4135018100bdbceabf6d21ecf4b
Scofieldsu <931180361@qq.com>
2017-06-23 15:30:35 +0800

    update test data

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # encoding: utf-8
 
 DEBUG = True
-PARAMS_TYPE = ['dict','int','str','float','list']
+PARAMS_TYPE = ['dict','int','str','float','list','datetime']
--- controller/api/base_api.py
+++ controller/api/base_api.py
@@ -7,24 +7,28 @@ api_add = api.dispatcher.add_method
 
 
 @api_add
-def test_api(param_dict, param_int, param_str, param_list):
+def test_api(my_dict, my_int, my_str, my_list, my_datetime):
     """
     :description  测试接口
-    :param param_dict: dict:字典参数
-    :param param_int: int:整型
-    :param param_str: str:无默认值
-    :param param_list: list:可以省略[]
+    :param my_dict: dict:字典参数
+    :param my_int: int:整型
+    :param my_str: str:无默认值
+    :param my_list: list:可以省略[]
+    :param my_datetime: datetime :时间戳
     :return: code or message
     """
 
-    data1 = json.loads(param_dict)
-    data2 = param_int
-    data3 = param_str
-    data4 = param_list
+    data1 = json.loads(my_dict)
+    data2 = my_int
+    data3 = my_str
+    data4 = my_list
+    data5 = my_datetime
     result = {
-        "param_dict": data1,
-        "param_int": data2,
-        "param_str": data3,
-        "param_list": data4
+        "loads之前的my_dict": my_dict,
+        "my_dict": data1,
+        "my_int": data2,
+        "my_str": data3,
+        "my_list": data4,
+        "my_datetime": data5
     }
     return result
