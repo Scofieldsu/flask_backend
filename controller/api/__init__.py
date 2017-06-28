584e611655b3d7f3cab1fa4ed01b3db11b6deb3e
Scofieldsu <931180361@qq.com>
2017-06-28 14:47:46 +0800

    add api_class

--- controller/api/__init__.py
+++ controller/api/__init__.py
@@ -7,6 +7,7 @@ from jsonrpc.dispatcher import Dispatcher
 
 
 class MyJsonRpcApi(JSONRPCAPI):
+
     def as_blueprint(self, name=None, url='/'):
         blueprint = Blueprint(name if name else str(uuid4()), __name__)
         blueprint.add_url_rule(
@@ -15,6 +16,8 @@ class MyJsonRpcApi(JSONRPCAPI):
             '/map', view_func=self.jsonrpc_map, methods=['GET'])
         return blueprint
 
+
 dispatcher = Dispatcher()
 api = MyJsonRpcApi(dispatcher)
 api_add = api.dispatcher.add_method
+api_class = api.dispatcher.add_class
--- controller/api/loginout.py
+++ controller/api/loginout.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-from controller.api.base_api import api_add
+from controller.api import api_add,api_class
 
 
 @api_add
@@ -22,3 +22,26 @@ def logout(name):
     :return: 退出信息success or error
     """
     return "logout success"
+
+
+@api_class
+class UserApi(object):
+
+    def login_test(self, name, pwd):
+        """
+        :description test登录接口
+        :param name: str
+        :param pwd: str
+        :return: test登录信息
+        """
+        result = {"msg": "login success", "code": 200}
+        return result
+
+
+    def logout_test(self, name):
+        """
+        :description test退出接口
+        :param name: str
+        :return: test退出信息success or error
+        """
+        return "logout success"
